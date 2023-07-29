76421cf9101c625372c0d2d9459a2461f1cb3999
waveofmymind <sjun0913@gmail.com>
2023-07-29 20:25:10 +0900

    fix: Auditing 안되는 문제 해결

--- src/main/java/waveofmymind/wanted/global/config/JpaConfig.java
+++ src/main/java/waveofmymind/wanted/global/config/JpaConfig.java
@@ -1,7 +1,8 @@
 package waveofmymind.wanted.global.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
+@Configuration
 @EnableJpaAuditing
 public class JpaConfig {
 }
