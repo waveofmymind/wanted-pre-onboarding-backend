1e5524c8428d4e5d0bf8700e133056e790b483dc
waveofmymind <sjun0913@gmail.com>
2023-07-29 13:18:52 +0900

    feat: 유저는 authenticate 함수로 식별될 수 있다.

--- src/test/java/waveofmymind/wanted/domain/user/domain/UserTest.java
+++ src/test/java/waveofmymind/wanted/domain/user/domain/UserTest.java
@@ -2,8 +2,10 @@ package waveofmymind.wanted.domain.user.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import waveofmymind.wanted.global.security.PasswordEncryptor;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class UserTest {
 
@@ -12,14 +14,30 @@ public class UserTest {
     void createUserTest() {
         //given
         String email = "test@test.com";
-        String password = "12345678";
+        Password password = new Password("12345678");
         //when
         User user = User.builder()
-                    .email(email)
-                    .password(password)
-                    .build();
+                .email(email)
+                .password(password)
+                .build();
         //then
         assertThat(user.getEmail()).isEqualTo(email);
         assertThat(user.getPassword()).isEqualTo(password);
+    }
+
+    @DisplayName("유저는 패스워드로 식별된다.")
+    @Test
+    void userIsIdentifiedByPassword() {
+        //given
+        String email = "test@test.com";
+        Password password = new Password("12345678");
+        //when
+        User user = User.builder()
+                .email(email)
+                .password(password)
+                .build();
+        Password requestPassword = new Password("12345678");
+        //then
+        assertDoesNotThrow(() -> user.authenticate(requestPassword));
     }
 }
