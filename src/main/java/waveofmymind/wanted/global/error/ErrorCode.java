55ce3d721c4ef6f31c4e811c9c7169a99bb9efea
waveofmymind <sjun0913@gmail.com>
2023-07-30 11:23:51 +0900

    feat: 조회에 대한 예외 응답 코드로 Not Found(404)를 발생시킨다.

--- src/main/java/waveofmymind/wanted/global/error/ErrorCode.java
+++ src/main/java/waveofmymind/wanted/global/error/ErrorCode.java
@@ -9,12 +9,12 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     AUTHENTICATION_FAILED(HttpStatus.UNAUTHORIZED, "사용자 정보가 일치하지 않습니다."),
-    USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "사용자를 찾을 수 없습니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
     DUPLICATE_JOIN(HttpStatus.BAD_REQUEST, "이미 가입된 사용자입니다."),
     INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "올바른 형식이 아닙니다."),
     INVALID_PASSWORD_PARAMETER(HttpStatus.BAD_REQUEST, "올바른 비밀번호 형식이 아닙니다."),
     TOKEN_VERIFY_FAILED(HttpStatus.BAD_REQUEST, "토큰 검증에 실패했습니다."),
-    ARTICLE_NOT_FOUND(HttpStatus.BAD_REQUEST, "게시글을 찾을 수 없습니다."),
+    ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND, "게시글을 찾을 수 없습니다."),
     UN_AUTHORIZED(HttpStatus.UNAUTHORIZED, "권한이 없습니다.");
 
     private final HttpStatus status;
